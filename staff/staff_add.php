93f6f8256e8faa8226ec2ff65fe06693b9f69ec1
ymgc15 <kayo_yamaguchi@docomo.ne.jp>
2022-07-21 22:51:00 +0900

    061

--- staff/staff_add.php
+++ staff/staff_add.php
@@ -4,15 +4,22 @@
  <meta charset="UTF-8">
  <meta http-equiv="X-UA-Compatible" content="IE=edge">
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
- <title>あいうえお</title>
+ <title>オンラインストア</title>
 </head>
 <body>
   スタッフ追加<br>
-
+  <br>
   <form method="post" action="staff_add_check.php">
-   やまぐち<br>
-   <input type="text" name="name" style="width:200px"><br>
-   1234<br>
+  スタッフ名を入力してください。<br>
+  <input type="text" name="name" style="width:200px"><br>
+  パスワードを入力してください。<br>
+  <input type="password" name="pass" style="width:100px"><br>
+  パスワードをもう一度入力してください。<br>
+  <input type="password" name="pass" style="width:100px"><br>
+  <br>
+  <input type="button" onclick="history.back()" value="戻る">
+  <input type="submit" value="ＯＫ">
   </form>
+
 </body>
 </html>
