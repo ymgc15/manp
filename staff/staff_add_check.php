d530b5507fc4aab5c1b3002abfe9e2672eed4717
ymgc15 <kayo_yamaguchi@docomo.ne.jp>
2022-07-22 16:41:39 +0900

    064

--- staff/staff_add_check.php
+++ staff/staff_add_check.php
@@ -44,7 +44,7 @@
       print '<input type="hidden" name="name" value="'.$staff_name.'">';
       print '<input type="hidden" name="pass" value="'.$staff_pass.'">';
       print '<br>';
-      print '<input type="button" name="name" value="戻る">';
+      print '<input type="button" onclick="history.back()" value="戻る">';
       print '<input type="submit" value="ＯＫ">';
       print '</form>';
     }
