d972f25def7e5e1afe84aa0db43301c612f354a5
ymgc15 <kayo_yamaguchi@docomo.ne.jp>
2022-07-24 15:43:03 +0900

    099

--- staff/staff_branch.php
+++ staff/staff_branch.php
@@ -2,14 +2,30 @@
 
   // もしditだったら修
   if(isset($_POST['edit']) == true) {
-    header('Location:staff_edit.php');
+
+    if(isset($_POST['staffcode']) == false) {
+      header('Location:staff_ng.php');
+      exit();
+    }
+
+    $staff_code = $_POST['staffcode'];
+    header('Location:staff_edit.php?staffcode='.$staff_code);
     exit();
+
   }
 
   // もしdeleteだったら削除
   if(isset($_POST['delete']) == true) {
-    header('Location:staff_delete.php');
+
+    if(isset($_POST['staffcode']) == false) {
+      header('Location:staff_ng.php');
+      exit();
+    }
+
+    $staff_code = $_POST['staffcode'];
+    header('Location:staff_delete.php?staffcode='.$staff_code);
     exit();
+
   }
 
 ?>
