63b130c175c189792c7d00a469946c6262e61f53
ymgc15 <kayo_yamaguchi@docomo.ne.jp>
2022-07-26 12:57:01 +0900

    129

--- product/pro_add.php
+++ product/pro_add.php
@@ -10,11 +10,15 @@
 
   商品追加<br>
     <br>
-    <form method="post" action="pro_add_check.php">
+    <!-- enctype="multipart/form-data"を忘れると、次の画面で画像の情報を受け取ることが出来なくなる -->
+    <form method="post" action="pro_add_check.php" enctype="multipart/form-data">
   商品名を入力してください。<br>
     <input type="text" name="name" style="width:200px"><br>
   価格を入力してください。<br>
     <input type="text" name="price" style="width:50px"><br>
+  画像を選んでください。<br>
+  <!-- typeをfileにすると、画像に限らずファイルを取り込めるようになる -->
+  <input type="file" name="gazou" style="width:400px"><br>
     <br>
     <input type="button" onclick="history.back()" value="戻る">
     <input type="submit" value="ＯＫ">
--- product/pro_add_check.php
+++ product/pro_add_check.php
@@ -13,6 +13,8 @@
     // 前の画面(product/pro_add.php)で入力されたデータを$_POSTから取り出して変数にコピーする。
     $pro_name = $_POST['name'];
     $pro_price = $_POST['price'];
+    // 受け取ったファイルの情報を取り出す
+    $pro_gazou = $_FILES['gazou'];
 
     // 以下の変数に安全対策(エスケープ処理)を施す
     // 以下の変数の中にある危険な文字を、安全な文字に置き換えて、同じ変数自身にコピーする。
@@ -41,8 +43,31 @@
       print '円<br>';
     }
 
+    // もし画像サイズが0より大きければ「画像あり」
+    // 画像が選択されていない場合、画像サイズは0バイトということ。最初のif命令でそれをチェックしている
+    // もし0バイトなら何もしない (画像データのない商品があっても良い)
+    // $pro_gazou['size'] … 画像のサイズ。単位はByte(バイト)
+    if($pro_gazou['size'] > 0) {
+
+      // if命令の中にまたif命令 → 「 入れ子 (ネスト) 」と呼ぶ
+      // もし画像サイズが1000000バイト(1MB)以上だった場合
+      if($pro_gazou['size'] > 1000000) {
+        print '画像が大き過ぎます';
+      } else {
+        // 画像を「gazouフォルダ」にアップロード
+        // move_uploaded_file(移動元,移動先) 順序：①画像ファイルは、既に$pro_gazou['tmp_name']が指し示すフォルダにある。②そこへアップロードされた時、サーバーによって勝手にファイル名が変えられる。③それを元のファイル名に戻して、[gazou]フォルダへ移動している。
+        // $pro_gazou['tmp_name'] … 仮にアップロードされている画像本体の場所と名前
+        // $pro_gazou['name'] … 画像のファイル名
+        move_uploaded_file($pro_gazou['tmp_name'],'./gazou/'.$pro_gazou['name']);
+        // アップロードした画像を表示
+        print '<img src="./gazou/'.$pro_gazou['name'].'">';
+        print '<br>';
+      }
+
+    }
+
     // もし上記の1つでも入力ミスがあれば、画面には「戻る」ボタンを表示し。商品追加入力画面に戻ってもらう。
-    if ($pro_name == ''|| preg_match('/^[0-9]+$/',$pro_price) == 0) {
+    if ($pro_name == ''|| preg_match('/^[0-9]+$/',$pro_price) == 0 || $pro_gazou['size'] > 1000000) {
       print '<form>';
       // history.back()はJavaScript言語で、入力したデータを消さずに前の画面に戻ることができ、かつボタンとして表示することが可能。
       // <a>タグでも前の画面に戻ることはできるが、入力したデータが画面から全て消えてしまうため打ち直しになる。
@@ -55,6 +80,8 @@
       print '<form method="post" action="pro_add_done.php">';
       print '<input type="hidden" name="name" value="'.$pro_name.'">';
       print '<input type="hidden" name="price" value="'.$pro_price.'">';
+      // 画像のファイル名をデータベースに登録するために、hiddenで次の画面に渡す
+      print '<input type="hidden" name="gazou_name" value="'.$pro_gazou['name'].'">';
       print '<br>';
       print '<input type="button" onclick="history.back()" value="戻る">';
       print '<input type="submit" value="ＯＫ">';
